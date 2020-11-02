11605021177f674108ccef355c6ea917404924bd
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-11-02 19:20:20 -0300

    Sequelize configurado

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'loacalhost',
+  username: 'postgres',
+  password: '123456',
+  database: 'goshaver',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
