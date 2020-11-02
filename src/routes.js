2163fb6684943b64fd609f87c7d05fd5948e4f6d
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-11-02 17:52:37 -0300

    Adicionado ESLint com style guide da Airbnb; configurações personalizadas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
+
 const routes = new Router();
 
-routes.get( '/', (_, res) => res.json({ message: "Hello Word" }) );
+routes.get('/', (_, res) => res.json({ message: 'Hello Word' }));
 
 module.exports = routes;
