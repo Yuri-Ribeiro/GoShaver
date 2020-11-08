b9419cf1cea6c7ce320601f8fb482e516ad6bb82
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-11-08 20:39:37 -0300

    Adicionado middleware de autenticação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,6 +10,11 @@ class UserController {
     const { id, name, email, provider } = await User.create(req.body);
 
     return res.json({ id, name, email, provider });
+  },
+
+  async update(req, res) {
+    console.log(req.userId)
+    return res.json({ ok: 'tudo certo' });
   }
 }
 
