8782d569721f70446480de248af161095a15a54f
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-11-08 11:12:25 -0300

    Criado controller para cadastro de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (_, res) => {
-  const user = await User.create({
-    name: 'Yuri Barros',
-    email: 'yuri2@yuri.com',
-    password_hash: '123456',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 module.exports = routes;
