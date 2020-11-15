c6ed83e643575910fdff928188ab5b54235eca0a
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-11-15 16:31:09 -0300

    Update de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,19 +2,36 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+    const newUser = req.body;
+    const userExists = await User.findOne({ where: { email: newUser.email } });
 
     if (userExists)
       return res.status(400).json({ error: 'User alrealdy exists.' });
 
-    const { id, name, email, provider } = await User.create(req.body);
+    const { id, name, email, provider } = await User.create(newUser);
 
     return res.json({ id, name, email, provider });
-  },
+  }
 
   async update(req, res) {
-    console.log(req.userId)
-    return res.json({ ok: 'tudo certo' });
+    const updatedUser = req.body;
+    const { email, oldPassword } = updatedUser;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists)
+        return res.status(401).json({ error: 'Email alrealdy registered.' });
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword)))
+      return res.status(401).json({ error: 'Password does not match.' });
+
+    const { id, name, provider } = await user.update(updatedUser);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -15,8 +15,8 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
-    next();
+    return next();
   } catch (err) {
-    return res.status(401).json({ error: 'Token invalid.' });
+    return res.status(401).json({ error: 'Invalid Token.' });
   }
 };
